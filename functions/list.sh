96b69eca86a67ba2f22b22b0215a507338859510
linqueta <lincolnrodrs@gmail.com>
2019-09-29 17:49:24 -0300

    add vscode

--- functions/list.sh
+++ functions/list.sh
@@ -1,6 +1,7 @@
 list=(
-  'UPDATE_PACKAGES;programs/update_packages.sh;true'
+  'UPDATE_PACKAGES;programs/update_packages.sh;false'
   'REQUIRED_LIBS;programs/required_libs.sh;without_command curl'
   'GIT;programs/git.sh;without_command git'
   'CHROME;programs/chrome.sh;without_source google-chrome arch=amd64'
+  'VSCODE;programs/vscode.sh;without_command code'
 )
---
+++ programs/vscode.sh
@@ -0,0 +1,8 @@
+cd
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+cd
